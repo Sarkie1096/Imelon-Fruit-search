92b9ed513c1334b4a5e2b6ecc642c3bba36fd4f3
Sarkie1096 <134732192+Sarkie1096@users.noreply.github.com>
2023-07-01 16:12:24 -0700

    Add files via upload

---
+++ Imelon.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('input');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+input.onkeyup= function () {
+	let results = [];
+let inputt= input.value;
+if(inputt.length) {
+    results= fruit.filter((keyword)=> {
+       return keyword.toLowerCase().includes(inputt.toLowerCase());
+    });
+    console.log(results);
+}
+showSuggestions(results);
+	
+}
+
+
+function showSuggestions(results) {
+const content= results.map((list)=>{
+    return "<li onclick=selectInput(this)>"+ list+ "</li>";
+});
+
+suggestions.innerHTML= "<ul>"+ content.join('') + "</ul>";
+	
+}
+
+function selectInput(list){
+    input.value= list.innerHTML;
+    suggestions.innerHTML= '';
+}
